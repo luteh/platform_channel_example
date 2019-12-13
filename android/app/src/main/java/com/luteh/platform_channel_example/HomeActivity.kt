010f933b088747d69bdad5081ffd37078e714221
luthfan.maftuh <luthfan.maftuh@uangteman.com>
2019-12-13 10:54:30 +0700

    Initial commit

---
+++ android/app/src/main/java/com/luteh/platform_channel_example/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.luteh.platform_channel_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
---
+++ android/app/src/main/kotlin/com/luteh/platform_channel_example/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.luteh.platform_channel_example
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "samples.flutter.dev/battery"
+
+    private val methodGetBatteryLevel = "getBatteryLevel"
+    private val methodGoToHomeScreen = "goToHomeScreen"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+
+        MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+            // Note: this method is invoked on the main thread.
+
+            when (call.method) {
+                methodGetBatteryLevel -> {
+                    val batteryLevel = getBatteryLevel()
+
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
+                }
+                methodGoToHomeScreen -> {
+                    goToHomeScreen()
+                    result.success(true)
+                }
+                else -> result.notImplemented()
+
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        batteryLevel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+
+    private fun goToHomeScreen() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+}
